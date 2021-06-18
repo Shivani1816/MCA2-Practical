6a9d7a33ab335adac859af1c1c3654250b7e34a8
Shivani1816 <85729644+Shivani1816@users.noreply.github.com>
2021-06-18 13:40:42 +0530

    Add files via upload

---
+++ MCA-2B/Shivani-20712052/Ques 2/Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Sum{
+	public static void main(String[] args){
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 3 numbers:");	    
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		int num3=sc.nextInt();    	
+		System.out.println("Sum = "+(num1+num2+num3));	
+	}
+
+
+}
